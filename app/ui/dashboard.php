efb42f91f5c749522a497e51f26edda3def9667f
Trevin Landers <trevinl@netixo.lk>
2018-04-01 19:50:18 +0530

    Dashboard - Modifications

--- app/ui/dashboard.php
+++ app/ui/dashboard.php
@@ -127,29 +127,33 @@ $sql3="SELECT COUNT(Status) FROM tbl_orders WHERE Createdby='$val54'";
    <div id="wrapper">
                <div class="row">
                    <div class="col-lg-3 col-md-6">
-                       <div class="panel panel-info">
+                       <div class="panel panel-default">
                            <div class="panel-heading">
                                <div class="row">
                                    <div class="col-xs-3">
-                                       <i class="fa fa-comments fa-5x"></i>
+                                     <span class="svg-48">
+                                <img src="assets/img/sf/handshake.svg" alt="handshake">
+                                    </span>
                                    </div>
                                    <div class="col-xs-9 text-right">
-                                       <div><h2 style="color:white"><?php echo $Sale; ?></h2></div>
-                                       <div>My Total Sales</div>
+                                       <div><h2><?php echo $Sale; ?></h2></div>
+                                       <div>Total Sales for the day </div>
                                    </div>
                                </div>
                            </div>
                        </div>
                    </div>
                     <div class="col-lg-3 col-md-6">
-                       <div class="panel panel-warning">
+                       <div class="panel panel-default">
                            <div class="panel-heading">
                                <div class="row">
                                    <div class="col-xs-3">
-                                       <i class="fa fa-shopping-cart fa-5x"></i>
+                                     <span class="svg-48">
+                                <img src="assets/img/sf/box-in.svg" alt="box-in">
+                                    </span>
                                    </div>
                                    <div class="col-xs-9 text-right">
-                                       <div><h2 style="color:white"><?php echo $new ?></h2></div>
+                                       <div><h2><?php echo $new ?></h2></div>
                                        <div>New Orders</div>
                                    </div>
                                </div>
@@ -157,30 +161,34 @@ $sql3="SELECT COUNT(Status) FROM tbl_orders WHERE Createdby='$val54'";
                        </div>
                    </div>
                    <div class="col-lg-3 col-md-6">
-                       <div class="panel panel-success">
+                       <div class="panel panel-default">
                            <div class="panel-heading">
                                <div class="row">
                                    <div class="col-xs-3">
-                                       <i class="fa fa-tasks fa-5x"></i>
+                                     <span class="svg-48">
+                                <img src="assets/img/sf/mixer.svg" alt="mixer">
+                                    </span>
                                    </div>
                                    <div class="col-xs-9 text-right">
-                                       <div><h2 style="color:white"><?php echo $id; ?></h2></div>
-                                       <div>Progressing Orders</div>
+                                       <div><h2><?php echo $id; ?></h2></div>
+                                       <div>Ongoing Projects</div>
                                    </div>
                                </div>
                            </div>
                        </div>
                    </div>
                    <div class="col-lg-3 col-md-6">
-                       <div class="panel panel-danger">
+                       <div class="panel panel-default">
                            <div class="panel-heading">
                                <div class="row">
                                    <div class="col-xs-3">
-                                       <i class="fa fa-support fa-5x"></i>
+                                     <span class="svg-48">
+                                <img src="assets/img/sf/globe.svg" alt="globe">
+                                    </span>
                                    </div>
                                    <div class="col-xs-9 text-right">
-                                       <div><h2 style="color:white"><?php echo $active; ?></h2></div>
-                                       <div>Active Services</div>
+                                       <div><h2><?php echo $active; ?></h2></div>
+                                       <div>Live Services</div>
                                    </div>
                                </div>
                            </div>
@@ -192,321 +200,12 @@ $sql3="SELECT COUNT(Status) FROM tbl_orders WHERE Createdby='$val54'";
                    <div class="col-lg-4">
                        <div class="panel panel-default">
                            <div class="panel-heading">
-                               <h3 class="panel-title"><i class="fa fa-long-arrow-right fa-fw"></i> Knowledge Base</h3>
+                               <h3 class="panel-title">  <img src="assets/img/sf/bullhorn.svg" alt="bullhorn"> Notice Board</h3>
                            </div>
                            <div class="panel-body">
                               <div class="list-group">
-                   <a href="" class="list-group-item " data-toggle="modal" data-target="#myModal">
-                  <h4 class="list-group-item-heading">Netixo Web Products and Pricing</h4>
-                 <p class="list-group-item-text">You can find our existing Web Plans and pricing details through here.</p>
-                  </a>
-                  <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
- <div class="modal-dialog modal-lg" role="document">
-   <div class="modal-content">
-     <div class="modal-header">
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-       <h4 class="modal-title" id="myModalLabel">Netixo Product Pricing</h4>
-     </div>
-     <div class="modal-body">
- <h2>Web Designing Pricing</h2>
- <p><b>Alwasys make sure to mention<u style="color:red"> only the total price</u> to the customer.</b>
- <br />
- <div class="alert alert-info" role="alert">
- <ul>
-   <li>Domain and Hosting Prices will be renewed Anually. Prices mentioned below is per year.</li>
-   <li>Payments can be done directly to Netixo Technologies.</li>
-   <li>Price might change if the client decide to go ahead with International domains</li>
- </ul>
-   </div>
- </p>
- <br/>
- <div class="row">
- <div class="col-sm-6 col-md-4">
 
-    <div class="panel panel-success">
-     <div class="panel-heading">
-       <h3 class="panel-title">Lite Package</h3>
-     </div>
-     <div class="panel-body">
-             <br/>
-            <table class="table table-striped">
-       <tr>
-         <td>Static Web Site - 5 Pages</td>
-         <td>Rs.10,000</td>
-       </tr>
-       <tr>
-         <td>.lk Domain</td>
-         <td>Rs.3,575</td>
-       </tr>
-       <tr>
-         <td>1GB hosting space with 100GB bandwith</td>
-         <td>Rs.2,925</td>
-       </tr>
-       <tr>
-         <td>Upto 4 email accounts</td>
-         <td>Free</td>
-       </tr>
-       <tr>
-         <td>Search Engine Optimization</td>
-         <td>Free</td>
-       </tr>
-     </table>
-     </div>
-     <div class="panel-footer">
-       <p align="center"style="color: #F0080C; font-size:16px;"><b>Total Price LKR 16,500/-</b></p>
-     </div>
- </div>
 
- </div>
- <div class="col-sm-6 col-md-4">
-
-    <div class="panel panel-success">
-     <div class="panel-heading">
-       <h3 class="panel-title">Elite Package</h3>
-     </div>
-     <div class="panel-body">
-         <br/>
-            <table class="table table-striped">
-       <tr>
-         <td>Static Web Site - 10 Pages</td>
-         <td>Rs.20,000</td>
-       </tr>
-       <tr>
-         <td>.lk Domain</td>
-         <td>Rs.3,575</td>
-       </tr>
-       <tr>
-         <td>2GB hosting space with 100GB bandwith</td>
-         <td>Rs.4,425</td>
-       </tr>
-       <tr>
-         <td>Upto 10 email accounts</td>
-         <td>Free</td>
-       </tr>
-       <tr>
-         <td>Search Engine Optimization</td>
-         <td>Free</td>
-       </tr>
-     </table>
-     </div>
-     <div class="panel-footer">
-       <p align="center"style="color: #F0080C; font-size:16px;"><b>Total Price LKR 28,000/-</b></p>
-     </div>
- </div>
-
- </div>
-
- <div class="col-sm-6 col-md-4">
-
-    <div class="panel panel-success">
-     <div class="panel-heading">
-       <h3 class="panel-title">Scroll.S Package </h3>
-     </div>
-     <div class="panel-body">
-   <br/>
-            <table class="table table-striped">
-       <tr>
-         <td>Static Web Page with 5 Sections
-</td>
-         <td>Rs.7,000</td>
-       </tr>
-       <tr>
-         <td>.lk Domain</td>
-         <td>Rs.3,575</td>
-       </tr>
-       <tr>
-         <td>1GB hosting space with 100GB bandwith</td>
-         <td>Rs.2,925</td>
-       </tr>
-       <tr>
-         <td>Upto 4 email accounts</td>
-         <td>Free</td>
-       </tr>
-       <tr>
-         <td>Search Engine Optimization</td>
-         <td>Free</td>
-       </tr>
-     </table>
-     <br />
-     </div>
-     <div class="panel-footer">
-       <p align="center"style="color: #F0080C; font-size:16px;"><b>Total Price LKR 13,500/-</b></p>
-     </div>
- </div>
-
- </div>
-</div>
-<br />
-
-<div class="row">
- <div class="col-sm-6 col-md-4">
-
-    <div class="panel panel-info">
-     <div class="panel-heading">
-       <h3 class="panel-title">Drogon Plus</h3>
-     </div>
-     <div class="panel-body">
-             <br/>
-            <table class="table table-striped">
-       <tr>
-         <td>Content Management System</td>
-         <td>Rs.20,000</td>
-       </tr>
-       <tr>
-         <td>Dynamic Web Site - 5 Pages</td>
-         <td>Rs.10,000</td>
-       </tr>
-       <tr>
-         <td>.lk Domain</td>
-         <td>Rs.3,575</td>
-       </tr>
-       <tr>
-         <td>1GB hosting space with 100GB bandwith</td>
-         <td>Rs.2,925</td>
-       </tr>
-       <tr>
-         <td>Upto 4 email accounts</td>
-         <td>Free</td>
-       </tr>
-       <tr>
-         <td>Search Engine Optimization</td>
-         <td>Free</td>
-       </tr>
-     </table>
-     </div>
-     <div class="panel-footer">
-       <p align="center"style="color: #F0080C; font-size:16px;"><b>Total Price LKR 36,500/-</b></p>
-     </div>
- </div>
- </div>
-
- <div class="col-sm-6 col-md-4">
-
-    <div class="panel panel-info">
-     <div class="panel-heading">
-       <h3 class="panel-title">Drogon Lite</h3>
-     </div>
-     <div class="panel-body">
-   <br/>
-            <table class="table table-striped">
-               <tr>
-         <td>Content Management System</td>
-         <td>Rs.20,000</td>
-       </tr>
-       <tr>
-         <td>Dynamic Web Site with 5 Sections</td>
-         <td>Rs.7,000</td>
-       </tr>
-       <tr>
-         <td>.lk Domain</td>
-         <td>Rs.3,575</td>
-       </tr>
-       <tr>
-         <td>1GB hosting space with 100GB bandwith</td>
-         <td>Rs.2,925</td>
-       </tr>
-       <tr>
-         <td>Upto 4 email accounts</td>
-         <td>Free</td>
-       </tr>
-       <tr>
-         <td>Search Engine Optimization</td>
-         <td>Free</td>
-       </tr>
-     </table>
-     </div>
-     <div class="panel-footer">
-       <p align="center"style="color: #F0080C; font-size:16px;"><b>Total Price LKR 33,500/-</b></p>
-     </div>
- </div>
-
- </div>
-</div>
-     </div>
-     <div class="modal-footer">
-       <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-     </div>
-   </div>
- </div>
-</div> <!-- end of Modal -->
-
-                 <a href="" class="list-group-item" data-toggle="modal" data-target="#myModal2">
-                  <h4 class="list-group-item-heading">Domain Price List</h4>
-                 <p class="list-group-item-text">You can find the price list for the both Local and International domains here.</p>
-                  </a>
-                 <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
- <div class="modal-dialog" role="document">
-   <div class="modal-content">
-     <div class="modal-header">
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-       <h4 class="modal-title" id="myModalLabel">Domain Price List</h4>
-     </div>
-     <div class="modal-body">
-      <p>With effect from 15th July 2017, following rates apply for domain registration and renewals.</p>
-
-      <br />
-       <table class="table table-striped">
-       <tr>
-         <td>TLD</td>
-         <td>Registration</td>
-         <td>Renewal</td>
-       </tr>
-       <tr>
-         <td>.com</td>
-         <td>Rs 1650.00</td>
-         <td>Rs 1750.00</td>
-       </tr>
-       <tr>
-         <td>.net</td>
-         <td>Rs 1750.00</td>
-         <td>Rs 1870.00</td>
-       </tr>
-       <tr>
-         <td>.org</td>
-         <td>Rs 1870.00</td>
-         <td>Rs 1980.00</td>
-       </tr>
-       <tr>
-         <td>.biz</td>
-         <td>Rs 1650.00</td>
-         <td>Rs 1750.00</td>
-       </tr>
-       <tr>
-         <td>.info</td>
-         <td>Rs 1750.00</td>
-         <td>Rs 1870.00</td>
-       </tr>
-     </table>
-     </div>
-     <div class="modal-footer">
-       <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-     </div>
-   </div>
- </div>
-</div> <!-- end of Modal -->
-
-<a href="" class="list-group-item " data-toggle="modal" data-target="#myModal3">
-                  <h4 class="list-group-item-heading" style="color: #FF0004">Ongoing Promotions</h4>
-                 <p class="list-group-item-text">You can find the details of the ongoing promotions here.</p>
-                  </a>
-                 <div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
- <div class="modal-dialog modal-lg" role="document">
-   <div class="modal-content">
-     <div class="modal-header">
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-       <h4 class="modal-title" id="myModalLabel">Ongoing Promotions</h4>
-     </div>
-     <div class="modal-body">
-    <p> There are no ongoing promotions at the moment.</p>
-     </div>
-
-     <div class="modal-footer">
-       <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-     </div>
-       </div>
-   </div>
-</div> <!-- end of Modal -->
 
                </div>
                            </div>
