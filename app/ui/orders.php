a2c19d809d9b581baa46596a9a818d119685f5ea
Trevin Landers <trevinl@netixo.lk>
2018-06-26 21:13:35 +0530

    Order Form Modification Completed.

--- app/ui/orders.php
+++ app/ui/orders.php
@@ -434,42 +434,28 @@ ol.progress-track li.progress-todo .icon-wrap .icon-down-arrow {
               <br/ >
               <div class="col-md-12">
                 <!-- Add Notes Section -->
-                <div class="col-md-6">
-                  <h3><b>Add Quick Notes</b></h3>
-                  <br />
-                  <div class="panel panel-default">
-                      <div class="panel-body">
-                      <form action="" method="post">
-                           <input type="hidden" id="disabledTextInput" class="form-control" name="txtref" value="<?php if(isset($ordernumb)){ echo($ordernumb); } ?>">
-                           <label for="disabledTextInput">Add Comment</label>
-                            <textarea class="form-control" rows="3" name="txtcomment"></textarea><br />
-                            <div align="center">
-                            <button type="submit" class="btn btn-success" name="btn_addNote" >Save Comment</button></div>
-                          </form>
-                          <?php
-                          $notes = $_POST ['txtcomment'];
-                          $val = $_SESSION ['user'];
-                          $Ref = $_POST['txtref'];
-
-                          if(isset ($_POST['btn_addNote']))
-                          {
-                          $sql3 = "insert into tbl_ordernotes(Nref,Username,comments) values ('$Ref','$val','$notes')";
-
-                          if (mysqli_query($connect,$sql3))
-                          {
-
-                          }
-                          else{
-                          $resu = mysqli_error($connect);
-                          echo $resu;
-                          }
-                          }
-                          ?>
-
-    </div>
-</div>
+                <div class="col-md-12">
+                  <h3><b>Credits & Approvals</b></h3>
+                        <br />
+                        <div class="responsive-table">
+        <table class="table table-striped table-bordered" width="100%" cellspacing="0">
+        <thead>
+        <tr>
+        <th>Date and Time</th>
+        <th>Requested By</th>
+        <th>Remarks</th>
+        <th>Status</th>
+        <th>Approved By</th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+      </div>
                 </div>
+
                 <!-- End of Add Notes Section -->
+                <div class="row">
                 <div class="col-md-6">
                 <!-- Order Notes Section -->
                 <h3><b>Project History</b></h3>
@@ -506,6 +492,26 @@ ol.progress-track li.progress-todo .icon-wrap .icon-down-arrow {
 </div>
                 <!-- end of Order Notes -->
               </div>
+              <div class="col-md-6">
+                <h3><b>Configuration Change Requests</b></h3>
+                <br />
+                      <div class="responsive-table">
+      <table class="table table-striped table-bordered" width="100%" cellspacing="0">
+      <thead>
+      <tr>
+      <th>Requested Date</th>
+      <th>Current Plan</th>
+      <th>New Plan</th>
+      <th>Status</th>
+      </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+    </div>
+
+              </div>
+            </div>
               </div>
             </div>
             <div class="col-md-12 article-v1-footer">
@@ -521,41 +527,3 @@ ol.progress-track li.progress-todo .icon-wrap .icon-down-arrow {
             </div>
         </div>
     </div>
-    <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                <h4 class="modal-title" id="myModalLabel">
-                                    Modal title
-                                    <a class="anchorjs-link" href="#myModalLabel"><span class="anchorjs-icon"></span></a>
-                                </h4>
-                            </div>
-                            <div class="modal-body">
-                                <h4 id="text-in-a-modal">
-                                    Text in a modal
-                                    <a class="anchorjs-link" href="#text-in-a-modal"><span class="anchorjs-icon"></span></a>
-                                </h4>
-                                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-                                <hr>
-                                <h4 id="popover-in-a-modal">
-                                    Popover in a modal
-                                    <a class="anchorjs-link" href="#popover-in-a-modal"><span class="anchorjs-icon"></span></a>
-                                </h4>
-                                <p>This <a href="#" role="button" class="btn btn-default popover-test" title="" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title" aria-describedby="popover786510">button</a><div class="popover fade right in" role="tooltip" id="popover786510" style="top: 114.5px; left: 121.734px; display: block;"><div class="arrow" style="top: 50%;"></div><h3 class="popover-title">A Title</h3><div class="popover-content">And here's some amazing content. It's very engaging. right?</div></div> should trigger a popover on click.</p>
-                                <hr>
-                                <h4 id="tooltips-in-a-modal">
-                                    Tooltips in a modal
-                                    <a class="anchorjs-link" href="#tooltips-in-a-modal"><span class="anchorjs-icon"></span></a>
-                                </h4>
-                                <p><a href="#" class="tooltip-test" title="" data-original-title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="" data-original-title="Tooltip">that link</a> should have tooltips on hover.</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                        <!-- /.modal-content -->
-                    </div>
-                    <!-- /.modal-dialog -->
-                </div>
